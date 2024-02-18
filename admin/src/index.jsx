65266ac4c9c7a7a20e435ee67991b4d6f34519c3
Kollos2k <kollos@vorsicht-bissig.de>
2024-02-18 19:00:59 +0100

    initial commit

--- admin/src/index.jsx
+++ admin/src/index.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import theme from "@iobroker/adapter-react/Theme";
-import Utils from "@iobroker/adapter-react/Components/Utils";
+import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
+import * as serviceWorker from "./serviceWorker.js";
+import theme from "@iobroker/adapter-react-v5/Theme";
+import Utils from "@iobroker/adapter-react-v5/Components/Utils";
 import App from "./app";
+import pkg from "../../package.json";
 
 let themeName = Utils.getThemeName();
+// console.log(`${pkg.name}@${pkg.version} using theme "${themeName}"`);
 
 function build() {
 	ReactDOM.render(
-		<MuiThemeProvider theme={theme(themeName)}>
-			<App
-				adapterName="vis-2-shelly"
-				onThemeChange={(_theme) => {
-					themeName = _theme;
-					build();
-				}}
-			/>
-		</MuiThemeProvider>,
+		<StyledEngineProvider injectFirst>
+			<ThemeProvider theme={theme(themeName)}>
+				<App
+					onThemeChange={(_theme) => {
+						themeName = _theme;
+						build();
+					}}
+				/>
+			</ThemeProvider>
+		</StyledEngineProvider>,
 		document.getElementById("root"),
 	);
 }
 
 build();
+
+serviceWorker.unregister();
