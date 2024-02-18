4d9031d808aba7c69facd16ad12278b9d2fb2c34
Kollos2k <kollos@vorsicht-bissig.de>
2024-02-18 19:00:39 +0100

    Initial Commit

--- main.js
+++ main.js
@@ -20,107 +20,231 @@ class Vis2Shelly extends utils.Adapter {
 			...options,
 			name: "vis-2-shelly",
 		});
+		this.isUnloaded = false;
 		this.on("ready", this.onReady.bind(this));
+		this.on("objectChange", this.onObjectChange.bind(this));
 		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
 		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
+		this.setState("info.connection", false, true);
 	}
 
 	/**
 	 * Is called when databases are connected and adapter received configuration.
 	 */
 	async onReady() {
+		await this.subscribeForeignObjectsAsync("shelly.*");
+		await this.subscribeObjectsAsync("rooms.*");
 		// Initialize your adapter here
 
-		// Reset the connection indicator during startup
-		this.setState("info.connection", false, true);
+		// await this.setForeignObjectAsync("vis-shelly.0", {
+		// 	type: "meta",
+		// 	common: {
+		// 		"name": {
+		// 		  "en": "Meta storage for user files1",
+		// 		  "de": "Meta-Speicher für Benutzerdateien",
+		// 		  "ru": "Meta Storage для файлов пользователей",
+		// 		  "pt": "Meta de armazenamento para arquivos de usuário",
+		// 		  "nl": "Meta opslag voor gebruikersbestanden",
+		// 		  "fr": "Stockage Meta pour les fichiers utilisateur",
+		// 		  "it": "Meta storage per i file utente",
+		// 		  "es": "Meta almacenamiento para archivos de usuario",
+		// 		  "pl": "Meta storage for user files",
+		// 		  "uk": "Зберігання мета для файлів користувачів",
+		// 		  "zh-cn": "用户档案的储存"
+		// 		},
+		// 		"type": "meta.user"
+		// 	},
+		// 	native: {},
+		// });
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		// this.log.info("config option1: " + this.config.option1);
+		// this.log.info("config option2: " + this.config.option2);
+		this.log.info("RÄUME");
+		this.log.info(JSON.stringify(this.config["rooms"]));
+		this.updateDeviceList();
+		this.setState("info.connection", true, true);
+	}
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
+	async updateDeviceList() {
+		const shellyDevices = await this.getForeignObjectsAsync("shelly.*", "device");
+		const keysDevices = Object.keys(shellyDevices);
+		const devJSON = [];
+
+		await this.setObjectNotExistsAsync("devices.ids", {
 			type: "state",
 			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
+				name: "Shelly DeviceList",
+				type: "array",
+				role: "shelly.deviceList",
 				read: true,
 				write: true,
 			},
 			native: {},
 		});
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
+		for (const deviceID of keysDevices) {
+			await this.setObjectNotExistsAsync(deviceID, {
+				type: "device",
+				common: {
+					name: deviceID,
+				},
+				native: {},
+			});
+			const deviceName = deviceID.substring(deviceID.lastIndexOf(".") + 1, deviceID.length);
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+			await this.setObjectNotExistsAsync("devices." + deviceName, {
+				type: "device",
+				common: {
+					name: deviceName,
+				},
+				native: {},
+			});
+			/** Create DEVICE TYPE */
+			await this.setObjectNotExistsAsync("devices." + deviceName + ".type", {
+				type: "state",
+				common: {
+					name: deviceName + ".type",
+					type: "string",
+					role: "name",
+					read: true,
+					write: true,
+				},
+				native: {},
+			});
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
-
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+			/** GET RELAY COUNT example: plus2pm with more than 1 relay */
+			let relayCount = 1;
+			const typeState = await this.getForeignStateAsync(deviceID + ".type");
+			if (typeState != null) {
+				this.setState("devices." + deviceName + ".type", { val: typeState.val, ack: true });
+				devJSON.push({ stateId: deviceID, id: deviceName, type: typeState.val });
+				if (typeState.val == "shellyplus2pm") relayCount = 2;
+			}
+			/** CREATE vis-shelly DEVICE FROM shelly OBJECTS*/
+			for (let i = 0; i < relayCount; i++) {
+				await this.setObjectNotExistsAsync("devices." + deviceName + "." + i, {
+					type: "device",
+					common: {
+						name: "Device Realay " + deviceName,
+					},
+					native: {},
+				});
+				/** Set Name */
+				this.setObjectNotExists(
+					"devices." + deviceName + "." + i + ".name",
+					{
+						type: "state",
+						common: {
+							name: deviceName + ".name",
+							type: "string",
+							role: "name",
+							read: true,
+							write: true,
+							def: null,
+						},
+						native: {},
+					},
+					() => {
+						/* Update default Name */
+						this.getState(`devices.${deviceName}.${i}.name`, (err, state) => {
+							if (state == null) {
+								this.getForeignState(deviceID + ".name", (err, state) => {
+									let newName = "";
+									if (state == null || state.val == null || state.val == "") newName = deviceName;
+									else newName = state.val.toString();
+									if (relayCount > 1) newName += `-${i}`;
+									this.setState(`devices.${deviceName}.${i}.name`, { val: newName, ack: true });
+								});
+							}
+						});
+					},
+				);
+			}
+		}
+		await this.setStateAsync("devices.ids", { val: JSON.stringify(devJSON), ack: true });
+		this.updateRoomsList();
+		this.log.info("Devices updated");
 	}
 
+	async updateRoomsList() {
+		const roomEnum = {};
+		for (const roomKey of Object.keys(this.config["rooms"])) {
+			const curRoom = this.config["rooms"][roomKey];
+			roomEnum[curRoom.id] = curRoom.name;
+		}
+		const shellyDevices = await this.getObjectViewAsync("system", "device", {
+			startkey: `vis-shelly.${this.instance}.devices.`,
+			endkey: `vis-shelly.${this.instance}.devices.\u9999`,
+		});
+		for (const devKey of Object.keys(shellyDevices.rows)) {
+			const dev = shellyDevices.rows[devKey];
+
+			const devName = [...dev.id.matchAll(/[.]([^.]*[.][0-9]*)$/g)];
+			// this.log.info(JSON.stringify(devName))
+			if (devName.length == 0) continue;
+			// String(dev.id).substring(String(dev.id).lastIndexOf(".")+1);
+			// @ts-ignore
+			this.setObjectAsync("devices." + devName[0][1] + ".room", {
+				type: "state",
+				common: {
+					name: "room",
+					type: "multistate",
+					role: "name",
+					read: true,
+					write: true,
+					states: roomEnum,
+				},
+				native: {},
+			});
+		}
+
+		this.setObjectAsync("devices.roomIds", {
+			type: "state",
+			common: {
+				name: "room",
+				type: "array",
+				role: "name",
+				read: true,
+				write: false,
+			},
+			native: {},
+		});
+		await this.setStateAsync("devices.roomIds", { val: JSON.stringify(roomEnum), ack: true });
+	}
 	/**
 	 * Is called when adapter shuts down - callback has to be called under any circumstances!
 	 * @param {() => void} callback
 	 */
 	onUnload(callback) {
 		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
+			this.setState("info.connection", false, true);
+			this.log.info("cleaned everything up...");
 			callback();
 		} catch (e) {
 			callback();
 		}
 	}
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
+	/**
+	 * Is called if a subscribed object changes
+	 * @param {string} id
+	 * @param {ioBroker.Object | null | undefined} obj
+	 */
+	onObjectChange(id, obj) {
+		// this.log.info(id);
+		// this.log.info(JSON.stringify(obj));
+
+		if (id.indexOf("vis-shelly") > -1) {
+			if (id.indexOf(".rooms.") > -1) {
+				this.updateRoomsList();
+			}
+		} else {
+			if (obj) {
+				if (obj.type == "device") this.updateDeviceList();
+			}
+		}
+	}
 
 	/**
 	 * Is called if a subscribed state changes
@@ -137,23 +261,66 @@ class Vis2Shelly extends utils.Adapter {
 		}
 	}
 
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+	// createAndSetStateIfExists(foreignID, selfID, name, role) {
+	// 	this.getForeignState(foreignID, (err, state) => {
+	// 		if (typeof state.val != "undefined") {
+	// 			this.setObjectNotExists(
+	// 				selfID,
+	// 				{
+	// 					type: "state",
+	// 					common: {
+	// 						name: name,
+	// 						type: "string",
+	// 						role: role,
+	// 						read: true,
+	// 						write: true,
+	// 					},
+	// 					native: {},
+	// 				},
+	// 				() => {
+	// 					this.setState(selfID, { val: foreignID, ack: true });
+	// 				},
+	// 			);
 	// 		}
-	// 	}
+	// 	});
 	// }
+
+	createAndSetState(foreignID, selfID, name, role) {
+		this.setObjectNotExists(
+			selfID,
+			{
+				type: "state",
+				common: {
+					name: name,
+					type: "string",
+					role: role,
+					read: true,
+					write: true,
+				},
+				native: {},
+			},
+			() => {
+				this.setState(selfID, { val: foreignID, ack: true });
+			},
+		);
+	}
+
+	/**
+	 * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+	 * Using this method requires "common.message" property to be set to true in io-package.json
+	 * @param {ioBroker.Message} obj
+	 */
+	onMessage(obj) {
+		if (typeof obj === "object" && obj.message) {
+			if (obj.command === "send") {
+				// e.g. send email or pushover or whatever
+				this.log.info("send command");
+
+				// Send response in callback if required
+				if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+			}
+		}
+	}
 }
 
 if (require.main !== module) {
